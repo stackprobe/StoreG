7a59bab3b9e5a9b8d405dd3f010e14a323f942a7
stackprobe <stackprobes@gmail.com>
2023-05-06 10:04:45 +0900

    Voyager 1 is 23913833802.271 kilometers away from the Sun.

--- DevOld/Cron/Petra20200001/Program.c
+++ DevOld/Cron/Petra20200001/Program.c
@@ -3,6 +3,8 @@
 #include <time.h>
 #include <windows.h>
 
+// ---- error
+
 #define Error() \
 	Error_LN(__LINE__)
 
@@ -11,117 +13,137 @@ static void Error_LN(int lineno)
 	char command[1024]; // rough size
 
 	printf("FATAL %d\n", lineno);
-	sprintf(command, "START "" *[ERROR-%d]", lineno);
+	sprintf(command, "START *[ERROR-%d]", lineno);
 	system(command);
 	exit(1);
 }
 
-// ----
+// ---- utils
 
-static int GetMax(int a, int b)
+static int IsExistFile(char *file)
 {
-	return a < b ? b : a;
-}
-static int GetMin(int a, int b)
-{
-	return a < b ? a : b;
-}
+	FILE *fp = fopen(file, "rb");
 
-#define STOP_EV_NAME "{baf19612-401e-4417-9b1f-48b0b8b72501}_STOP_CRON"
-#define PERIOD_SEC 60
-
-static time_t CurrTime;
-
-static int IsFairBatch(char *batch)
-{
-	FILE *fp = fopen(batch, "rb");
-	int ret = 0;
-
-	if (!fp) // ? バッチファイルは存在しない。
-		goto endFunc_NC;
-
-	if (fgetc(fp) == EOF) // ? バッチファイルは空のファイル
-		goto endFunc;
-
-	ret = 1;
-
-endFunc:
-	if (fclose(fp) != 0)
+	if (fp && fclose(fp) != 0)
 		Error();
 
-endFunc_NC:
-	printf("IsFairBatch_ret: %d\n", ret);
-	return ret;
+	return !!fp;
 }
-static void RunIfTimeout(char *batch, int periodSec)
+static char *GetStrDateTime(time_t t)
 {
-	int remSec = (int)(CurrTime % periodSec);
+	static char str[26]; // size: ctime ret
 
-	printf("Batch: %s (%d, %d, %d)\n", batch, periodSec, remSec, periodSec - remSec);
+	strcpy(str, ctime(&t));
 
-	if (remSec == 0 && IsFairBatch(batch))
-		system(batch);
+	if (str[8] == ' ')
+		str[8] = '0';
 
-	printf("Batch_End\n");
+	str[24] = '\0';
+	return str;
 }
-static void RunIfTimeoutAll(void)
+
+// ---- main
+
+#define STOP_EV_NAME "Cron_STOP_{baf19612-401e-4417-9b1f-48b0b8b72501}"
+#define BATCH_NAME_MAX 12
+#define BATCH_MAX (4 * 99)
+
+typedef struct Batch_st
 {
-	printf("----\n");
-	printf("%s", ctime(&CurrTime));
+	char Name[BATCH_NAME_MAX];
+	int PeriodSec;
+	int RemainingSec;
+}
+Batch_t;
 
-	// 周期は PERIOD_SEC の倍数であること。
+static Batch_t Batches[BATCH_MAX];
+static int BatchCount;
 
-	RunIfTimeout(".\\Cron1m.bat"  , 60 * 1);
-	RunIfTimeout(".\\Cron3m.bat"  , 60 * 3);
-	RunIfTimeout(".\\Cron10m.bat" , 60 * 10);
-	RunIfTimeout(".\\Cron30m.bat" , 60 * 30);
-	RunIfTimeout(".\\Cron1h.bat"  , 3600 * 1);
-	RunIfTimeout(".\\Cron3h.bat"  , 3600 * 3);
-	RunIfTimeout(".\\Cron10h.bat" , 3600 * 10);
-	RunIfTimeout(".\\Cron30h.bat" , 3600 * 30);
-	RunIfTimeout(".\\Cron1d.bat"  , 86400 * 1);
-	RunIfTimeout(".\\Cron3d.bat"  , 86400 * 3);
-	RunIfTimeout(".\\Cron10d.bat" , 86400 * 10);
-	RunIfTimeout(".\\Cron30d.bat" , 86400 * 30);
+static void CollectBatch_SS(int suffix, int scale)
+{
+	char name[BATCH_NAME_MAX];
+	int count;
+
+	for (count = 1; count <= 99; count++)
+	{
+		sprintf(name, "Cron%d%c.bat", count, suffix);
+
+		if (IsExistFile(name))
+		{
+			Batch_t *i = Batches + BatchCount++;
+
+			strcpy(i->Name, name);
+			i->PeriodSec = count * scale;
+			i->RemainingSec = 0;
+		}
+	}
+}
+static void CollectBatch(void)
+{
+	CollectBatch_SS('s', 1);
+	CollectBatch_SS('m', 60);
+	CollectBatch_SS('h', 3600);
+	CollectBatch_SS('d', 86400);
+}
+static int ExecuteAllBatchIfTimeout(int elapsedSec)
+{
+	int nextWaitSec = 60; // rough limit
+	int index;
+
+	for (index = 0; index < BatchCount; index++)
+	{
+		Batch_t *i = Batches + index;
+
+		i->RemainingSec -= elapsedSec;
+
+		if (i->RemainingSec <= 0)
+		{
+			printf("# %s %s\n", GetStrDateTime(time(NULL)), i->Name);
+
+			system(i->Name);
+
+			printf("\n");
+
+			i->RemainingSec = i->PeriodSec;
+		}
+	}
+	for (index = 0; index < BatchCount; index++)
+	{
+		Batch_t *i = Batches + index;
+
+		if (nextWaitSec > i->RemainingSec)
+			nextWaitSec = i->RemainingSec;
+	}
+	return nextWaitSec;
 }
 void main(int argc, char **argv)
 {
 	HANDLE evStop = CreateEventA(NULL, 0, 0, STOP_EV_NAME);
-	int waitSec;
 
 	if (evStop == NULL)
 		Error();
 
 	if (argc == 2 && !_stricmp(argv[1], "/S"))
 	{
+		printf("Cron-STOP\n");
+
 		SetEvent(evStop);
-		printf("SetEvent_Stop\n");
-		goto endProc;
 	}
-	if (argc != 1) // ? 不明なコマンド引数が指定されている。
-		Error();
-
-	printf("ST_Cron\n");
-
-	CurrTime = 0;
-	RunIfTimeoutAll();
-	CurrTime = (time(NULL) / PERIOD_SEC) * PERIOD_SEC;
-
-	do
+	else
 	{
-		RunIfTimeoutAll();
-		CurrTime += PERIOD_SEC;
+		int waitSec = 0;
 
-		waitSec = CurrTime - time(NULL);
-		waitSec = GetMax(waitSec, PERIOD_SEC - PERIOD_SEC / 2);
-		waitSec = GetMin(waitSec, PERIOD_SEC + PERIOD_SEC / 2);
+		if (argc != 1) // ? 不明なコマンド引数が指定されている。
+			Error();
 
-		printf("waitSec: %d\n", waitSec);
+		CollectBatch();
+
+		printf("Cron-START\n");
+
+		while (WaitForSingleObject(evStop, waitSec * 1000) == WAIT_TIMEOUT)
+			waitSec = ExecuteAllBatchIfTimeout(waitSec);
+
+		printf("Cron-END\n");
 	}
-	while (WaitForSingleObject(evStop, waitSec * 1000) == WAIT_TIMEOUT);
-
-	printf("ED_Cron\n");
-
-endProc:
 	CloseHandle(evStop);
 }
